201c96cd062a66f56caca9cb2c156b011921e74a
Jonathan Conn <jonathansconn@gmail.com>
2023-04-20 17:17:30 -0400

    Fixed the nested <a><a></a></a> issue by creating a custom "link" with a next-ui Text element and some css styling

--- examples/wallet/src/components/SessionCard.tsx
+++ examples/wallet/src/components/SessionCard.tsx
@@ -2,21 +2,23 @@ import { truncate } from "@/utils/HelperUtil";
 import { Avatar, Card, Link, Text } from "@nextui-org/react";
 import Image from "next/image";
 import NextLink from "next/link";
+import { useRouter } from "next/router";
 
 /**
  * Types
  */
 interface IProps {
-  topic?: string
-  logo?: string
-  name?: string
-  url?: string
+  topic?: string;
+  logo?: string;
+  name?: string;
+  url?: string;
 }
 
 /**
  * Component
  */
 export default function SessionCard({ logo, name, url, topic }: IProps) {
+  const router = useRouter();
   return (
     <NextLink href={topic ? `/session?topic=${topic}` : "#"} passHref>
       <Card
@@ -40,9 +42,14 @@ export default function SessionCard({ logo, name, url, topic }: IProps) {
             <Text h5 css={{ marginLeft: "$9" }}>
               {name}
             </Text>
-            <Link href={url} css={{ marginLeft: "$9" }}>
+            <Text
+              css={{ marginLeft: "$9", color: "$link" }}
+              onClick={() => {
+                if (url) router.push(url);
+              }}
+            >
               {truncate(url?.split("https://")[1] ?? "Unknown", 23)}
-            </Link>
+            </Text>
           </div>
 
           <Image src={"/icons/arrow-right-icon.svg"} width={20} height={20} alt="session icon" />
