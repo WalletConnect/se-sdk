d03d56ca3aca7c9d3196cbbfbbcc2d32af355998
Gancho Radkov <ganchoradkov@gmail.com>
2023-05-25 17:58:44 +0300

    chore: hardcode tx params

--- examples/dapp/src/helpers/tx.ts
+++ examples/dapp/src/helpers/tx.ts
@@ -13,7 +13,15 @@ export async function formatTestTransaction(account: string, transactionCount: n
   const _value = 0;
   const value = encoding.sanitizeHex(encoding.numberToHex(_value));
 
-  const tx = { from: address, to: address, data: "0x", nonce, gasLimit, gasPrice: "", value };
+  // const tx = { from: address, to: address, data: "0x", nonce, gasLimit, gasPrice: "", value };
 
-  return tx;
+  return {
+    from: "0x13A2Ff792037AA2cd77fE1f4B522921ac59a9C52",
+    to: "0x13A2Ff792037AA2cd77fE1f4B522921ac59a9C52",
+    data: "0x",
+    nonce: "0x01",
+    gasPrice: "0x0afe4146dc",
+    gasLimit: "0x5208",
+    value: "0x00",
+  };
 }
