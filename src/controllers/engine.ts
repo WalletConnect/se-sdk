2d23812d67c3abae8586bbb6c7ae3df1b815fdd8
Gancho Radkov <ganchoradkov@gmail.com>
2023-07-05 10:02:22 +0300

    fix: ternary check

--- src/controllers/engine.ts
+++ src/controllers/engine.ts
@@ -64,14 +64,12 @@ export class Engine extends ISingleEthereumEngine {
           ...normalizedRequired[EVM_IDENTIFIER],
           accounts: approvedChains.map((chain) => formatAccounts(accounts, chain)).flat(),
           chains: approvedChains.map((chain) => prefixChainWithNamespace(chain)),
-          methods: normalizedRequired[EVM_IDENTIFIER]?.methods?.length || [
-            "eth_sendTransaction",
-            "personal_sign",
-          ],
-          events: normalizedRequired[EVM_IDENTIFIER]?.events?.length || [
-            "chainChanged",
-            "accountsChanged",
-          ],
+          methods: normalizedRequired[EVM_IDENTIFIER]?.methods?.length
+            ? normalizedRequired[EVM_IDENTIFIER].methods
+            : ["eth_sendTransaction", "personal_sign"],
+          events: normalizedRequired[EVM_IDENTIFIER]?.events?.length
+            ? normalizedRequired[EVM_IDENTIFIER].events
+            : ["chainChanged", "accountsChanged"],
         },
       },
     };
